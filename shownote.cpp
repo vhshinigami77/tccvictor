98da45c91f1af7503d95c4d3c9f4f266c0a05e8b
vhshinigami77 <61999227+vhshinigami77@users.noreply.github.com>
2025-08-01 01:02:11 -0300

    Update shownote.cpp

--- shownote.cpp
+++ shownote.cpp
@@ -4,55 +4,48 @@
 
 int main()
 {
-    FILE *entrada;
-    entrada = fopen ("resultado_saida.txt", "r");
-    
-    double fn;
-    double amplitude;
-    double limiar = 2e-3; 
-    fscanf (entrada, "%le %le", &fn, &amplitude);
-    fclose (entrada);
-    
-    FILE *saida;
-    saida = fopen ("nota.txt", "w");
-
-    //
-    if(amplitude < limiar){
-        printf("PAUSA...\n");
-        exit (0);
+    FILE *entrada = fopen("resultado_saida.txt", "r");
+    if (!entrada) {
+        fprintf(stderr, "Erro: não foi possível abrir resultado_saida.txt\n");
+        return 1;
     }
 
-    char notas[12][10];
-    sprintf (notas[0],"C");
-    sprintf (notas[1],"C#");
-    sprintf (notas[2],"D");
-    sprintf (notas[3],"D#");
-    sprintf (notas[4],"E");
-    sprintf (notas[5],"F");
-    sprintf (notas[6],"F#");
-    sprintf (notas[7],"G");
-    sprintf (notas[8],"G#");
-    sprintf (notas[9],"A");
-    sprintf (notas[10],"A#");
-    sprintf (notas[11],"B");
-    //
-    
-    double n;
-    int r;
-    int q;
-    
-    //
-    n = 12*log(fn/440)/log (2);
-    q = (int)(round (n+9))/12;
-    r = (int)(round (n+9))%12;
-    
-//    printf ("%lf %d %d\n", n, q, r);
-    printf ("%s%d", notas[r],4+q);
-    
-    
-    fprintf (saida, "%s%d", notas[r],4+q);
-    fclose (saida);
-    printf("\n");
+    double fn, amplitude;
+    double limiar = 2e-3;
+
+    if (fscanf(entrada, "%le %le", &fn, &amplitude) != 2) {
+        fprintf(stderr, "Erro: formato inesperado em resultado_saida.txt\n");
+        fclose(entrada);
+        return 1;
+    }
+    fclose(entrada);
+
+    FILE *saida = fopen("nota.txt", "w");
+    if (!saida) {
+        fprintf(stderr, "Erro: não foi possível criar nota.txt\n");
+        return 1;
+    }
+
+    if (amplitude < limiar) {
+        fprintf(saida, "PAUSA");
+        fclose(saida);
+        printf("PAUSA...\n");
+        return 0;
+    }
+
+    const char* notas[12] = {
+        "C", "C#", "D", "D#", "E", "F", "F#",
+        "G", "G#", "A", "A#", "B"
+    };
+
+    double n = 12 * log(fn / 440.0) / log(2);
+    int semitones = round(n + 9);
+    int q = semitones / 12;
+    int r = semitones % 12;
+
+    printf("%s%d\n", notas[r], 4 + q);
+    fprintf(saida, "%s%d", notas[r], 4 + q);
+    fclose(saida);
 
     return 0;
 }
